f4d422586bdcbe8434ab1dabaedb05200989c0f2
PaulaE <soypaulaespitia@gmail.com>
2025-08-28 12:38:00 -0500

    comandos git

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,18 @@ public class Main {
 
         System.out.println("hello word");
 
+        System.out.println("Aprendiendo git ;)");
+        System.out.println("Comandos de Git");
+        System.out.println("git status = estado de la rama");
+        System.out.println("git add . = agrupar archivos");
+        System.out.println("git commit -m (entre comillas dobles) = guardar el comentario ");
+        System.out.println("git push origin main = subir a la rama principal");
+        System.out.println("git clone = clonar el repo");
+        System.out.println("git branch = ver la ramas locales");
+        System.out.println("git fetch -v = ver ramas remotas");
+        System.out.println("git switch -c = crear nueva rama y posicionarse en ella");
+
+
+
     }
 }
